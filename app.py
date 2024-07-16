70f8f2ebaca75d272d2fe2994a14427347489e10
Emily Page <epageone81@gmail.com>
2024-07-16 18:07:13 +0000

    app.py

--- app.py
+++ app.py
@@ -194,4 +194,4 @@ def delete_vegie(vegie_id):
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
